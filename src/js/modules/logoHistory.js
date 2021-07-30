94cca393beeca51d250e5bac353a1dcf3f3aae4b
Alexei <1996Leha@mail.ru>
2021-07-30 10:42:31 +0500

    небольшой debug

--- src/js/modules/logoHistory.js
+++ src/js/modules/logoHistory.js
@@ -1,17 +1,27 @@
-const logoLink = document.querySelector('.logo a');
+import { cardsWrapper } from "../services/cardRender";
+import { showCards } from "./showCards";
+
+const logoLink = document.querySelector('.logo a'),
+      spinner = document.querySelector('.spinner'),
+      forms = document.querySelectorAll('form'),
+      search = document.querySelector('.search'),
+      cards = document.querySelector('.cards'),
+      company = document.querySelector('.company');
+
 // перезаписываем функцию клика по логотипу
 function pushHistoryLogo() {
     logoLink.onclick = event => {
         event.preventDefault();
         history.pushState(null, null);
-
-        const search = document.querySelector('.search'),
-            cards = document.querySelector('.cards'),
-            company = document.querySelector('.company');
-
         company.innerHTML = '';
         search.style.display = 'grid';
         cards.style.display = 'block';
+        cardsWrapper.innerHTML = '';
+        spinner.style.display = 'block';
+        forms.forEach(item => {
+            item.reset();
+        });
+        showCards();
         replaceHistoryLogo();
     };
 }
@@ -20,14 +30,12 @@ function replaceHistoryLogo() {
     logoLink.onclick = event => {
         event.preventDefault();
         history.replaceState(null, null);
-
-        const search = document.querySelector('.search'),
-            cards = document.querySelector('.cards'),
-            company = document.querySelector('.company');
-
-        company.innerHTML = '';
-        search.style.display = 'grid';
-        cards.style.display = 'block';
+        spinner.style.display = 'block';
+        cardsWrapper.innerHTML = '';
+        forms.forEach(item => {
+            item.reset();
+        });
+        showCards();
     };
 }
 
